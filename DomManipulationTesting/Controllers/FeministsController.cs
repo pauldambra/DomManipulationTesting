0b8573b3da847c59a15e254e751a9b6c5f6f1656
pauldambra <paul.dambra@gmail.com>
2013-07-29 22:45:43 +0100

    Initial pass at UI testing

---
+++ DomManipulationTesting/Controllers/FeministsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DomManipulationTesting.Controllers
+{
+    public class FeministsController : ApiController
+    {
+        private readonly string[] _feminists = {
+                "Marie De France",
+                "Eleanor of Aquitaine",
+                "Bettisia Gozzadini",
+                "Nicola De Lay Haye",
+                "Christine de Pizan",
+                "Jadwiga of Poland",
+                "Laura Cereta",
+                "La Malinche"
+            }; 
+
+        // GET api/feminists
+        public IEnumerable<string> Get()
+        {
+            return _feminists;
+        }
+
+        // GET api/feminists/5
+        public string Get(int id)
+        {
+            if (id < 0 || id > _feminists.Length)
+            {
+                //really want this to be 422 not 400
+                Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return _feminists.ElementAt(id);
+        }
+    }
+}
---
+++ DomManipulationTesting/Scripts/application.js
@@ -0,0 +1,29 @@
+﻿window.Feminists = window.Feminists || {
+    listing: []
+};
+
+Feminists.load = function() {
+    $.get(
+        '/api/feminists',
+        function (result) {
+            Feminists.listing = result;
+            Feminists.populateList(result);
+        },
+        'json'
+    );
+};
+
+Feminists.populateList = function () {
+    $('#result-container').empty();
+    for (var i = 0; i < Feminists.listing.length; i++) {
+        $('#result-container').append('<li>' + Feminists.listing[i] + '</li>');
+    }       
+};
+
+Feminists.shuffle = function () {
+    if (Feminists.listing.length > 0) {
+        var first = Feminists.listing.shift();
+        Feminists.listing.push(first);
+    }
+    Feminists.populateList();
+};
